1c2c5cd152782eb9b022cb54764670a71ee31394
rifie536 <maitakaz536@gmail.com>
2024-07-12 07:13:38 +0900

    音声オフ時にリセット音声が再生される現象を解消

--- arrangedCounter/reset.js
+++ arrangedCounter/reset.js
@@ -2,12 +2,16 @@
     const $counter = document.getElementById("js-counter");
     const body = document.body;
     const mario = document.getElementById("mario");
+    const volumeSlider = document.getElementById("volume-slider");
     const fanfare = document.getElementById("fanfare");
     const bgm = document.getElementsByClassName("bgm");
     // const bgm2 = document.getElementById("bgm-2");
     // const bgm3 = document.getElementById("bgm-3");
     const congratsMessage = document.getElementById("congrats-message");
     const quizContainer = document.getElementById("quiz-container");
+    const soundToggle = document.getElementById("sound-toggle");
+
+    let previousVolume = volumeSlider.value;
 
     const clickHandler = () => {
         $counter.textContent = 0;
@@ -22,6 +26,7 @@
     const playMario = () => {
         mario.pause();
         mario.currentTime = 0;
+        mario.volume = volumeSlider.value;
         mario.play();
     }
 
