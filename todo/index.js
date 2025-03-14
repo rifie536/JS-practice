a9c718c2ea432a29e92c7068bc4f1b699ea85709
rifie536 <maitakaz536@gmail.com>
2025-03-15 06:28:29 +0900

    todo追加機能作成

--- todo/index.js
+++ todo/index.js
@@ -1,5 +1,20 @@
 const onClickAdd = () => {
-    alert();
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    div.appendChild(p);
+    li.appendChild(div);
+
+    document.getElementById("incomplete-list").appendChild(li);
+
 }
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
